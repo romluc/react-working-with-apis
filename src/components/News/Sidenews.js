161a4db46f4a62eb7bfd88b76e361b96ba43c03d
romluc <romulofilhofilho@gmail.com>
2020-04-09 11:57:53 -0300

    Create Sidenews components

--- src/components/News/Sidenews.js
+++ src/components/News/Sidenews.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import SingleSide from './SingleSide';
+import axios from 'axios';
 
 class Sidenews extends Component {
   constructor(props) {
@@ -9,10 +10,24 @@ class Sidenews extends Component {
     };
   }
 
-  componentDidMount() {}
+  componentDidMount() {
+    const { type, query } = this.props.section;
+
+    const url = `http://newsapi.org/v2/${type}?${query}&apiKey=8d36c39ab3d44e2f80e8e7b00153de06
+      `;
+
+    axios
+      .get(url)
+      .then((res) => {
+        this.setState({
+          sidenews: res.data.articles,
+        });
+      })
+      .catch((err) => console.log(err));
+  }
 
   renderItems() {
-    return this.state.news.map((item) => (
+    return this.state.sidenews.map((item) => (
       <SingleSide key={item.url} item={item} />
     ));
   }
