7e8989fb36fdd6b5a6b79f22fc95bff795fe302e
romluc <romulofilhofilho@gmail.com>
2020-04-09 11:42:07 -0300

    Create SingleSide component

--- src/components/News/Sidenews.js
+++ src/components/News/Sidenews.js
@@ -1,1 +1,25 @@
+import React, { Component } from 'react';
+import SingleSide from './SingleSide';
 
+class Sidenews extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      sidenews: [],
+    };
+  }
+
+  componentDidMount() {}
+
+  renderItems() {
+    return this.state.news.map((item) => (
+      <SingleSide key={item.url} item={item} />
+    ));
+  }
+
+  render() {
+    return <div>{this.renderItems()}</div>;
+  }
+}
+
+export default Sidenews;
